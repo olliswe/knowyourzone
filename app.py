b580fb70b315cdd52d3897bc09d8a5171a14bf46
olliswe <oliveriyer@gmail.com>
2019-05-04 14:47:25 +0000

    hi

--- app.py
+++ app.py
@@ -2,16 +2,28 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
-import geojson
-import tempfile
+import json
 
 
 app = Flask(__name__)
 
+gdf = gpd.read_file('FCC Ward ShapleFile/Western_Area_Urban_Wards.shp')
+list = []
+for index, row in gdf.iterrows():
+    ward = int(row['Ward_No'])
+    dict = {"type": "Feature",
+            "geometry":row['geometry'].__geo_interface__,
+            "properties":{
+            "ward":ward}}
+    list.append(dict)
+data = {"type":"FeatureCollection",
+       "features":list}
+
+
 
 @app.route('/', methods= ['GET', 'POST'])
 def geo():
-    return render_template('index.html')
+    return render_template('index.html', layer=data)
 
 @app.route('/postmethod', methods=['GET','POST'])
 def postmethod():
@@ -41,5 +53,5 @@ def postmethod():
 #
 
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=80,debug=True)
+# if __name__ == '__main__':
+#     app.run(host='localhost', port=80,debug=True)
