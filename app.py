3e67bc55431d3eec8f21e7bbbd68296f8ad160c4
olliswe <oliveriyer@gmail.com>
2019-05-05 14:33:13 +0000

    added pan to to dropdown

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify
+     render_template, flash, jsonify, send_from_directory
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import json
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 gdf = gpd.read_file('FCC Ward ShapleFile/Western_Area_Urban_Wards.shp')
+gdf['coords'] = gdf['geometry'].apply(lambda x: x.representative_point().coords[:])
+gdf['coords'] = [coords[0] for coords in gdf['coords']]
+pos_list=[]
+for index, row in gdf.iterrows():
+    pos_list.append({'lng':row['coords'][0], 'lat':row['coords'][1] })
+gdf['pos']=pos_list
 list = []
 for index, row in gdf.iterrows():
     ward = int(row['Ward_No'])
     dict = {"type": "Feature",
             "geometry":row['geometry'].__geo_interface__,
             "properties":{
-            "ward":ward}}
+            "ward":ward,
+            "center":row['pos']}}
     list.append(dict)
 data = {"type":"FeatureCollection",
        "features":list}
-
 
 
 @app.route('/', methods= ['GET', 'POST'])
@@ -50,7 +56,9 @@ def postmethod():
         return jsonify(result = 'Nothing')
 
 
-#
+@app.route('/<path:filename>')
+def send_file(filename):
+    return send_from_directory(app.static_folder, filename)
 
 
 if __name__ == '__main__':
